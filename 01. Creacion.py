14f5af3c2996353457f99e8f452f5e6267bccb50
AlexisPE-15 <56988142+AlexisPE-15@users.noreply.github.com>
2019-11-07 17:57:13 -0500

    Add files via upload

---
+++ 01. Creacion.py
@@ -0,0 +1,41 @@
+# Ejercicio 1
+D = dict()
+print(D, type(D))
+
+# Ejercicio 2
+D = {}
+print(D, type(D))
+
+# Ejercicio 3
+D = dict(x = 1)
+print(D, type(D))
+
+# Ejercicio 4
+D = {1: "Hola", 2: "Hello"}
+print(D, type(D))
+
+# Ejercicio 5
+D = dict({2: "a"})
+print(D, type(D))
+
+# Ejercicio 6
+p = "a"
+q = "b"
+D = dict(zip(p,q))
+print(D, type(D))
+
+# Ejercicio 7
+D = {"Fruta": "manzana", "Vegetal": "Zanahoria"}
+print(D, type(D))
+
+# Ejercicio 8
+D = dict(A= "Hola")
+print(D, type(D))
+
+# Ejercicio 9
+D = {1: "celular", 2: "laptop"}
+print(D, type(D))
+
+# Ejercicio 10
+D = {"a": "ayuda"}
+print(D, type(D))
