bbcdc0323207ba554b0407785cd0d1547bae6964
AlexisPE-15 <56988142+AlexisPE-15@users.noreply.github.com>
2019-11-07 17:57:59 -0500

    Add files via upload

---
+++ 02. Pertenencia de clave.py
@@ -0,0 +1,31 @@
+Diccionario = {"a": "Teclado", "b": "Mouse", "c": "Pantalla", "d": "CPU", "e": "Bateria", "f": "Cargador", "g": "USB", "h": "Windows"}
+
+# Ejercicio 1
+print("a" in Diccionario)
+
+# Ejercicio 2
+print("j" in Diccionario)
+
+# Ejercicio 3
+print("b" not in Diccionario)
+
+# Ejercicio 4
+print("i" not in Diccionario)
+
+# Ejercicio 5
+print("e" in Diccionario)
+
+# Ejercicio 6
+print("g" not in Diccionario)
+
+# Ejercicio 7
+print("c" in Diccionario)
+
+# Ejercicio 8
+print("e" in Diccionario)
+
+# Ejercicio 9
+print("f" not in Diccionario)
+
+# Ejercicio 10
+print("z" in Diccionario)
---
+++ 04. Comparacion.py
@@ -0,0 +1,49 @@
+# Ejercicio 1
+D = {"a": "Teclado"}
+D1 = {"a": "Teclado"}
+print(D == D1)
+
+# Ejercicio 2
+D = {"b": "Mouse"}
+D1 = {"c": "mochila"}
+print(D == D1)
+
+# Ejercicio 3
+D = {"c": "Pantalla"}
+D1 = {"c": "Pantalla"}
+print(D != D1)
+
+# Ejercicio 4
+D = {"g": "USB"}
+D1 = {"g": "USB"}
+print(D == D1)
+
+# Ejercicio 5
+D = {"d": "CPU"}
+D1 = {"h": "taza"}
+print(D == D1)
+
+# Ejercicio 6
+D = {"e": "Bateria"}
+D1 = {1: "celular"}
+print(D != D1)
+
+# Ejercicio 7
+D = {"nota": 12}
+D1 = {"nota": 12}
+print(D != D1)
+
+# Ejercicio 8
+D = {1: "Hola"}
+D1 = {2: "hello"}
+print(D == D1)
+
+# Ejercicio 9
+D = {"Z": 27}
+D1 = {"a": 1}
+print(D != D1)
+
+# Ejercicio 10
+D = {10: "ayuda"}
+D1 = {10: "ayuda"}
+print(D != D1)
